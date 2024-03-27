617836215800152e290096c2376d9c74ed8c3a81
Huangwenjie <opoppopo0006@gmail.com>
2024-03-27 16:52:43 +0800

    add__all
    works,get in touch,footer.

---
+++ receive.php
@@ -0,0 +1,13 @@
+<?php
+echo 'Name:';
+echo '<br>';
+echo $_POST['Name'];
+echo '<br>';
+echo '<br>Email:';
+echo '<br>';
+echo $_POST['Email'];
+echo '<br>';
+echo '<br>Message:';
+echo '<br>';
+echo $_POST['Message'];
+echo '<br>';
